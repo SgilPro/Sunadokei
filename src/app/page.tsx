c68ccea74153e1c6734641863154d318b8cae9b3
Daniel Shih <daniel@bitstreetx.com>
2023-10-09 21:26:31 +0800

    feat: move profile content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,7 @@
 "use client";
-import React, { useEffect } from "react";
-
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/service/firebase";
-
-import SignOut from "@/components/SignOut";
-import TestData from "@/components/TestData";
-import User from "@/components/User";
-import { createTestData } from "@/repo/RecordRepo";
-import { useRouter } from "next/navigation";
+import React from "react";
 import MainLayout from "@/components/MainLayout";
-import NavBar from "@/components/NavBar";
 
 export default function Home() {
-  const [user] = useAuthState(auth);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!user) {
-      router.push("/signin");
-    }
-  }, [router, user]);
-
-  return (
-    <MainLayout>
-      <User user={user} />
-      {
-        <>
-          <TestData />
-          <button onClick={createTestData}>Create test data</button>
-          <SignOut />
-        </>
-      }
-    </MainLayout>
-  );
+  return <MainLayout>HOME</MainLayout>;
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,36 @@
+"use client";
+import React, { useEffect } from "react";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/service/firebase";
+
+import SignOut from "@/components/SignOut";
+import TestData from "@/components/TestData";
+import User from "@/components/User";
+import { createTestData } from "@/repo/RecordRepo";
+import { useRouter } from "next/navigation";
 import MainLayout from "@/components/MainLayout";
 
 export default function Profile() {
-  return <MainLayout>profile</MainLayout>;
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/signin");
+    }
+  }, [router, user]);
+
+  return (
+    <MainLayout>
+      <User user={user} />
+      {
+        <>
+          <TestData />
+          <button onClick={createTestData}>Create test data</button>
+          <SignOut />
+        </>
+      }
+    </MainLayout>
+  );
 }
